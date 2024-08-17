cc78bba8c3e03f0f27885d8f7966e06c5a2f825d
Rob Cheung <kousun12@gmail.com>
2024-08-17 17:41:52 -0400

    --wip-- [skip ci]

---
+++ rob_agi/aidertest.py
@@ -0,0 +1,19 @@
+from aider.coders import Coder
+from aider.models import Model
+
+# This is a list of files to add to the chat
+fnames = ["greeting.py"]
+
+model = Model("claude-3-5-sonnet-20240620")
+
+# Create a coder object
+coder: Coder = Coder.create(main_model=model, fnames=fnames)
+
+# This will execute one instruction on those files and then return
+coder.run("make a script that prints hello world")
+
+# Send another instruction
+coder.run("make it say goodbye")
+
+# You can run in-chat "/" commands too
+coder.run("/tokens")
