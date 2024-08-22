9e5380cad2f23d7bc893d192bf206f0925a05581
Rob Cheung <kousun12@gmail.com>
2024-08-22 11:15:37 -0400

    --wip-- [skip ci]

--- rob_agi/progressive_solve.py
+++ rob_agi/progressive_solve.py
@@ -11,6 +11,12 @@ from rob_agi.test_factory import run_pytest, setup_tests
 
 project_root = Path(__file__).parent.parent
 ignore_template = project_root / ".aiderignore"
+path = project_root / f"rob_agi/attempts/c_{challenge_id}"
+file_entries = {
+    "main": path / main_file,
+    "test": path / test_file,
+    "image": project_root / f"data/task_images/{challenge_id}.png",
+}
 
 # challenge_id = "c59eb873"  # easy
 challenge_id = "776ffc46"  # hard
@@ -20,50 +26,48 @@ challenges, solutions = load_task_set(task_set_name=task_set)
 main_file = "main.py"
 test_file = "test.py"
 
-adhoc_ignore = project_root / ".adhoc-aiderignore"
-with open(ignore_template, "r") as tf:
-    with open(adhoc_ignore, "w") as f:
-        f.write(tf.read())
-        f.write("\n")
-        f.write(f"!data/task_images/{challenge_id}.png\n")
-        f.write(f"!rob_agi/attempts/c_{challenge_id}\n")
 
-path = project_root / f"rob_agi/attempts/c_{challenge_id}"
-file_entries = {
-    "main": path / main_file,
-    "test": path / test_file,
-    "image": project_root / f"data/task_images/{challenge_id}.png",
-}
+def get_coder():
+    fnames = [file_entries["main"]]
+    read_only_fnames = [file_entries["test"]]
+    adhoc_ignore = project_root / ".adhoc-aiderignore"
+    with open(ignore_template, "r") as tf:
+        with open(adhoc_ignore, "w") as f:
+            f.write(tf.read())
+            f.write("\n")
+            f.write(f"!data/task_images/{challenge_id}.png\n")
+            f.write(f"!rob_agi/attempts/c_{challenge_id}\n")
+    io = InputOutput(chat_history_file=path / ".aider.chat.history.md", llm_history_file=path / ".aider.llm.history.md")
+    coder: Coder = Coder.create(
+        main_model=Model("claude-3-5-sonnet-20240620"),
+        fnames=fnames,
+        io=io,
+        read_only_fnames=read_only_fnames,
+        cache_prompts=True,
+        stream=False,
+        summarize_from_coder=False,
+        edit_format="ask",
+        auto_commits=False,
+        # max_reflections=3,
+        # auto_commits=False, use_git=False
+    )
+    coder.repo.aider_ignore_file = adhoc_ignore
+    # print(coder.repo.aider_ignore_file)
+    # print(coder.root)
+    # print(coder.get_all_relative_files())
+    # print(coder.get_repo_map())
+    return coder
 
-editable = [file_entries["main"]]
-setup_tests(challenge_id, task_set, path)
 
-model = Model("claude-3-5-sonnet-20240620")
-io = InputOutput(chat_history_file=path / ".aider.chat.history.md", llm_history_file=path / ".aider.llm.history.md")
-coder: Coder = Coder.create(
-    main_model=model,
-    fnames=editable,
-    io=io,
-    read_only_fnames=[file_entries["test"]],
-    cache_prompts=True,
-    stream=False,
-    # max_reflections=3,
-    # auto_commits=False, use_git=False
-)
-coder.repo.aider_ignore_file = adhoc_ignore
-# print(coder.repo.aider_ignore_file)
-# print(coder.root)
-# print(coder.get_all_relative_files())
-# print(coder.get_repo_map())
-
-current_result = run_pytest(file_entries["test"])
-success = current_result["success"]
 max_tries = 1
 tries = 0
 
 gp = challenges[challenge_id]
 goal = problem_setup_aider(gp)
 
+setup_tests(challenge_id, task_set, path)
+current_result = run_pytest(file_entries["test"])
+success = current_result["success"]
 print(current_result)
 
 while not success and tries < max_tries:
@@ -78,9 +82,15 @@ while not success and tries < max_tries:
     prompt += f"An image of the challenge is provided at {challenge_id}.png"
     coder.run(prompt)
     tries += 1
+    current_result = run_pytest(file_entries["test"])
+    success = current_result["success"]
+    print("SUCCESS: ", success)
 
-# delete the adhoc ignore file:
-adhoc_ignore.unlink(missing_ok=True)
+
+def teardown_coder():
+    # delete the adhoc ignore file:
+    adhoc_ignore.unlink(missing_ok=True)
+
 
 """
 Process should be:
