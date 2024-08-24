7288a781227cdb049e7d4b1225be206cdf0f3371
Rob Cheung <kousun12@gmail.com>
2024-08-24 17:26:38 -0400

    --wip-- [skip ci]

--- rob_agi/progressive_solve.py
+++ rob_agi/progressive_solve.py
@@ -41,7 +41,7 @@ class Solver:
             with open(self.adhoc_ignore, "w") as f:
                 f.write(tf.read())
                 f.write("\n")
-                f.write(f"!data/task_images/{self.challenge.id}.png\n")
+                # f.write(f"!data/task_images/{self.challenge.id}.png\n")
                 f.write(f"!rob_agi/attempts/c_{self.challenge.id}\n")
         setup_tests(self.challenge, self.solution, self.challenge_root)
         try:
@@ -50,11 +50,15 @@ class Solver:
             pass
 
     def get_coder(self, **kwargs):
+        ef = ""
+        if "edit_format" in kwargs:
+            ef = "-" + kwargs["edit_format"]
+
         io = InputOutput(
-            chat_history_file=self.challenge_root / ".aider.chat.history.md",
-            llm_history_file=self.challenge_root / ".aider.llm.history.md",
+            chat_history_file=self.challenge_root / f".aider.chat.history{ef}.md",
+            llm_history_file=self.challenge_root / f".aider.llm.history{ef}.md",
         )
-        io.yes = False
+        # io.yes = True
         coder: Coder = Coder.create(
             main_model=Model("claude-3-5-sonnet-20240620"),
             io=io,
@@ -73,12 +77,12 @@ class Solver:
         return coder
 
     def get_ask_coder(self):
-        fnames = [self.file_entries["main"], self.file_entries["test"], self.file_entries["image"]]
+        fnames = [self.file_entries["main"], self.file_entries["test"]]
         # fnames = [self.file_entries["main"], self.file_entries["test"]]
-        return self.get_coder(edit_format="ask", summarize_from_coder=False, fnames=fnames)
+        return self.get_coder(edit_format="ask", fnames=fnames)
 
     def get_modify_coder(self):
-        fnames = [self.file_entries["main"], self.file_entries["image"]]
+        fnames = [self.file_entries["main"]]
         read_only_fnames = [self.file_entries["test"], project_root / "rob_agi/colored_grid.py"]
         return self.get_coder(fnames=fnames, read_only_fnames=read_only_fnames)
 
@@ -89,48 +93,45 @@ class Solver:
 
     def get_prefix(self, is_first):
         if is_first:
-            prefix = f"{self.goal}\n\nCurrently the tests are failing. please fix the implementation. the tests never need to be modified."
+            prefix = f"{self.goal}\n\nCurrently the tests used to validate the solution are failing."
         else:
-            prefix = "The tests are still failing."
+            prefix = "The tests used to validate the solution are still failing."
         return prefix
 
     def get_plan(self, ask_coder, current_result, is_first=False):
         prefix = self.get_prefix(is_first)
-        prompt = f"{prefix}\n\n<VALIDATION_RESULTS>\n{current_result['error']}\n{current_result['output']}</VALIDATION_RESULTS>\n"
-        prompt += "Since the current implementation is incorrect, do not pay too much attention to it. Examine all the information and state your understanding of the challenge, and propose a solution to the challenge in words. Any solution must always apply to every case, not just the failing exception here.\n"
+        prompt = f"{prefix}\n\n<VALIDATION_OUTPUT>\n{current_result['error']}\n{current_result['output']}</VALIDATION_OUTPUT>\n"
+        prompt += "Examine all the information you have, state your understanding of the challenge, and propose a detailed solution to the challenge in words. Any solution must always apply to every case, not just the failing exception here.\n"
         prompt += "Then reflect on your idea. Look very closely and notice if there are any other patterns or discrepancies worth noting. Remember this is about identifying abstract, intuitive ideas about what is happening.\n"
         prompt += f"Explicitly consider how your idea applies to each of the examples and test cases in attempts/{self.challenge_id}/test.py. To check your thinking, illustrate how your idea either works or doesn't for each case.\n"
         prompt += f"If the rule(s) you came up with does not apply to any specific case, call it out and think about a more general idea that does apply in every case. Be meticulous and careful in your reflection. Sometimes you need to zoom out to see how a single idea can apply to all cases.\n"
         ask_coder.run(prompt)
-        ask_coder.run("Finally propose 3 distinct high level plans for how to solve this problem.\n")
 
-        res = ask_coder.run("From those ideas, choose the best, and detail a plan for how to implement the solution\n")
-
+        res = ask_coder.run("Based on that reflection detail a step by step plan for how to solve the challenge\n")
         return res
 
     def get_edit(self, modify_coder, current_result, plan, is_first=False):
         prefix = self.get_prefix(is_first)
-        prompt = f"{prefix}\n\n<VALIDATION_RESULTS>\n{current_result['error']}\n{current_result['output']}</VALIDATION_RESULTS>\n"
+        prompt = f"{prefix}\n\n<VALIDATION_OUTPUT>\n{current_result['error']}\n{current_result['output']}</VALIDATION_OUTPUT>\n"
         prompt += f"\nYour latest thinking is:\n<LATEST_THINKING>\n{plan}\n</LATEST_THINKING>\n"
         prompt += f"Use that thinking and solve the challenge by fixing the code. Always ensure that the docstring to solve_{self.challenge_id} includes a correct summary of the solution in words.\n"
         # prompt += f"An image of the challenge is provided at {self.challenge.id}.png"
         # prompt += f"colored_grid.py includes a library of functions that may be useful. modify this file if you need."
-        res = modify_coder.run(prompt)
-        return res
+        return modify_coder.run(prompt)
 
     def run_solve(self):
-        max_tries = 2
+        max_tries = 1
         tries = 0
         current_result = self.run_tests()
         is_failing = not current_result["success"]
 
         while is_failing and tries < max_tries:
+            print(f"-------------------- ATTEMPT {tries+1}/{max_tries} --------------------------\n")
             ask_coder = self.get_ask_coder()
             modify_coder = self.get_modify_coder()
             is_first = True
-            print(f"Try {tries+1}/{max_tries}")
             plan = self.get_plan(ask_coder, current_result, is_first=is_first)
-            res = self.get_edit(modify_coder, current_result, plan, is_first=is_first)
+            self.get_edit(modify_coder, current_result, plan, is_first=is_first)
             # print(modify_coder.aider_edited_files)
             tries += 1
             current_result = self.run_tests()
