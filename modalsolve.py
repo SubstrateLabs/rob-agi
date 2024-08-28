3fcb5375afe562cc7f273fc7c1bb39559008cecd
Rob Cheung <kousun12@gmail.com>
2024-08-28 16:44:25 -0400

    --wip-- [skip ci]

---
+++ modalsolve.py
@@ -0,0 +1,21 @@
+import modal
+
+image = (
+    modal.Image.debian_slim(python_version="3.10")
+    .apt_install("git")
+    .pip_install(
+        "pytest", "git+https://github.com/SubstrateLabs/rob-agi.git@4fe34707", "pydantic==2.8.2", "substrate", "numpy"
+    )
+)
+app = modal.App("arc_solver", image=image)
+
+
+@app.function()
+def foo():
+    return "hello"
+
+
+@app.local_entrypoint()
+def localfn():
+    h = foo.remote()
+    print("res", h)
--- msolve.py
+++ msolve.py
@@ -1,4 +1,3 @@
-import json
 import time
 from typing import Dict, List, Optional
 from pathlib import Path
