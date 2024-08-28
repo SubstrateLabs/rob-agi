b9162fe460b3c5890edddc13c7e35a4e87e249cb
Rob Cheung <kousun12@gmail.com>
2024-08-28 16:58:03 -0400

    --wip-- [skip ci]

--- modalsolve.py
+++ modalsolve.py
@@ -2,15 +2,19 @@ import modal
 
 image = (
     modal.Image.debian_slim(python_version="3.10")
+    .workdir("/app")
     .apt_install("git")
-    .pip_install(
-        "pytest", "git+https://github.com/SubstrateLabs/rob-agi.git@d85c3030", "pydantic==2.8.2", "substrate", "numpy"
-    )
+    .pip_install("pytest", "pydantic==2.8.2", "substrate", "numpy", "aider==0.54.0")
 )
 app = modal.App("arc_solver", image=image)
 
 
-@app.function()
+@app.function(
+    mounts=[
+        modal.Mount.from_local_dir("data", remote_path="/app/data"),
+        modal.Mount.from_local_dir("rob_agi", remote_path="/app/rob_agi"),
+    ]
+)
 def foo():
     return "hello"
 
